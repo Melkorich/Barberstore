ea62071a0b3195ce9e303fc7a23ce67b757dab29
Melkorich <Kirill.Lavrov92@gmail.com>
2021-10-17 01:14:41 +0300

    main page almost done

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,7 @@ $(function(){
     slidesToScroll: 2,
     arrows: false,
     dots: true,
+    infinity: true,
   });
 
   $('.catalog-form__option').styler();
@@ -15,6 +16,15 @@ $(function(){
     // ratedFill: "#D79468",
     readOnly: true,
   });
+
+  $('.products-tabs__btn').on('click', function(e) {
+    e.preventDefault();
+    $('.products-tabs__btn').removeClass('products-tabs__btn--active');
+    $(this).addClass('products-tabs__btn--active');
+
+    $('.products-tabs__item').removeClass('products-tabs__item--active');
+    $($(this).attr('href')).addClass('products-tabs__item--active');
+  })
 
   
  
